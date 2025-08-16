ba7cf423c95c67fa51cf76dd60798a0d5dfc3446
shannu211 <venkatashanmukhareddy.stexas@gmail.com>
2025-08-16 12:15:57 +0530

    remainder.py

---
+++ remainder.py
@@ -0,0 +1,11 @@
+ Simple Percentage Calculator in Python
+
+# Take input marks
+total_marks = int(input("Enter total marks: "))
+obtained_marks = int(input("Enter obtained marks: "))
+
+# Calculate percentage
+percentage = (obtained_marks / total_marks) * 100
+
+# Print result
+print("You got:", percentage, "%")
